cb89dc9dbb7e1904e3c926a0b941ba5167486276
奥田啓皓 <>
2022-06-19 13:30:31 +0900

    aboutページ、contactページのbaseを作成

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    echo "this is a about page";
+});
+
+Route::get('/contact', function () {
+    echo "this is a contact page";
+});
