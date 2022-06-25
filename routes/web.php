ad8045fbc127df6ec479470aad007b38135f3498
奥田啓皓 <>
2022-06-25 14:20:41 +0900

    home_sliderの登録を実装

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Route::controller(AdminController::class)->group(function () {
 
 Route::controller(HomeSliderController::class)->group(function(){
     Route::get('/home/slide','HomeSlider')->name('home.slide');
+    Route::post('/update/slider', 'UpdateSlider')->name('update.slider');
 });
 
 Route::get('/dashboard', function () {
