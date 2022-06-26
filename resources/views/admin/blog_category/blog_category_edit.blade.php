08f82cc738e49169099bfb4b11f84d63e800d005
奥田啓皓 <>
2022-06-26 16:33:12 +0900

    Blog CategoryのCRUDを実装

---
+++ resources/views/admin/blog_category/blog_category_edit.blade.php
@@ -0,0 +1,44 @@
+@extends('admin.admin_master')
+@section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<div class="page-content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+          <div class="card">
+              <div class="card-body">
+
+                  <h4 class="card-title">Blog Category Edit Page</h4>
+                  <form method="POST" action="{{route('update.blog.category', $blogcategory->id)}}">
+                    @csrf
+                    <div class="row mb-3">
+                      <label for="example-text-input" class="col-sm-2 col-form-label">Blog Category Name</label>
+                      <div class="col-sm-10">
+                          <input name="blog_category" class="form-control" type="text" id="example-text-input" value="{{$blogcategory->blog_category}}">
+                          @error('blog_category')
+                          <span class="text-danger">{{$message}}</span>
+                          @enderror
+                      </div>
+                    </div>
+                    <input type="submit" class="btn btn-info waves-effect waves-light" value="Update Blog Category Data">
+                  </form>
+                  <!-- end row -->
+              </div>
+          </div>
+      </div> <!-- end col -->
+  </div>
+  </div>
+</div>
+<script type="text/javascript">
+
+  $(document).ready(function(){
+    $("#image").change(function(e){
+      var reader = new FileReader();
+      reader.onload = function(e){
+        $('#showImage').attr('src',e.target.result);
+      }
+      reader.readAsDataURL(e.target.files['0']);
+    });
+  });
+</script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Home\HomeSliderController;
 use App\Http\Controllers\Home\AboutController;
 use App\Http\Controllers\Home\PortfolioController;
-
+use App\Http\Controllers\Home\BlogCategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -63,6 +63,14 @@ Route::controller(PortfolioController::class)->group(function(){
     Route::get('/portfolio/details/{id}', 'PortfolioDetails')->name('portfolio.details');
 });
 
+Route::controller(BlogCategoryController::class)->group(function(){
+    Route::get('/all/blog/category', 'AllBlogCategory')->name('all.blog.category');
+    Route::get('/add/blog/category', 'AddBlogCategory')->name('add.blog.category');
+    Route::post('/store/blog/category', 'StoreBlogCategory')->name('store.blog.category');
+    Route::get('/edit/blog/category/{id}', 'EditBlogCategory')->name('edit.blog.category');
+    Route::post('/update/blog/category/{id}', 'UpdateBlogCategory')->name('update.blog.category');
+    Route::get('/delete/blog/category/{id}','DeleteBlogCategory')->name('delete.blog.category');
+});
 Route::get('/dashboard', function () {
     return view('admin.index');
 })->middleware(['auth','verified'])->name('dashboard');
