f86444bab0c450c76a1a18e79364e3f1c8ea3bfa
奥田啓皓 <>
2022-06-27 14:27:43 +0900

    AdminControllerで設定したパスに、未ログインの状態ではアクセスできないようmiddlewareをつなげた

--- routes/web.php
+++ routes/web.php
@@ -31,13 +31,15 @@ Route::controller(DemoController::class)->group(function () {
     Route::get('/contact','ContactMethod')->name('contact.page');
 });
 
-Route::controller(AdminController::class)->group(function () {
-    Route::get('/admin/logout','destroy')->name('admin.logout');
-    Route::get('/admin/profile','Profile')->name('admin.profile');
-    Route::get('/edit/profile','EditProfile')->name('edit.profile');
-    Route::post('/store/profile', 'StoreProfile')->name('store.profile');
-    Route::get('/change/password', 'ChangePassword')->name('change.password');
-    Route::post('/update/password', 'UpdatePassword')->name('update.password');
+Route::middleware(['auth'])->group(function(){
+    Route::controller(AdminController::class)->group(function () {
+        Route::get('/admin/logout','destroy')->name('admin.logout');
+        Route::get('/admin/profile','Profile')->name('admin.profile');
+        Route::get('/edit/profile','EditProfile')->name('edit.profile');
+        Route::post('/store/profile', 'StoreProfile')->name('store.profile');
+        Route::get('/change/password', 'ChangePassword')->name('change.password');
+        Route::post('/update/password', 'UpdatePassword')->name('update.password');
+    });
 });
 
 Route::controller(HomeSliderController::class)->group(function(){
