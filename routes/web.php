25cf2e47318e1207ff8471b1db3a3dc0964e40a3
奥田啓皓 <>
2022-06-27 11:25:57 +0900

    Footer更新機能を実装

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Home\AboutController;
 use App\Http\Controllers\Home\PortfolioController;
 use App\Http\Controllers\Home\BlogCategoryController;
 use App\Http\Controllers\Home\BlogController;
+use App\Http\Controllers\Home\FooterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -83,6 +84,11 @@ Route::controller(BlogController::class)->group(function(){
     Route::get('/blog/details/{id}', 'BlogDetails')->name('blog.details');
     Route::get('/category/blog/{id}', 'CategoryBlog')->name('category.blog');
     Route::get('/blog', 'HomeBlog')->name('home.blog');
+});
+
+Route::controller(FooterController::class)->group(function(){
+   Route::get('/footer/setup','FooterSetup')->name('footer.setup');
+   Route::post('/update/footer','UpdateFooter')->name('update.footer');
 });
 Route::get('/dashboard', function () {
     return view('admin.index');
